4a202227396c262a54c5a0af6386cb1d0795cf2d
ryabinkin.d <ryabinkin.d@fasttech.dev>
2024-01-19 13:53:06 +0300

    feat: synpress metamask

--- synpress.config.js
+++ synpress.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000/',
+    baseUrl: 'https://crypto-manager-ashy.vercel.app/', //'http://localhost:3000/',
     specPattern: 'tests/e2e/specs',
     supportFile: 'tests/support/index.js',
     videosFolder: 'tests/e2e/videos',
